780a63b22c4d55e1c546f9ae4c4f0934bed47365
Tujiao Dong <xiongzhq1997@gmail.com>
2018-06-17 21:57:41 +0800

    color

--- Visualize.py
+++ Visualize.py
@@ -12,8 +12,8 @@ NS = 50
 
 pos = np.random.uniform(0, 1, (NS, 2))
 color = np.ones((NS, 4)) * (0, 0, 0, 1)
-color[:,0] = np.linspace(0, 1, NS)
-color[:,1] = np.linspace(0, 1, NS)
+color[:,0] = np.linspace(0, 0.5, NS)
+color[:,1] = np.linspace(0, 0.8, NS)
 color[:,2] = np.linspace(0, 1, NS)
 
 fig = plt.figure(num = "Bike Simulation", figsize=(6,6), facecolor='white')
