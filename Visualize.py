c73c478872f54f17d425544d4b86133679765d32
Tujiao Dong <xiongzhq1997@gmail.com>
2018-06-17 21:37:14 +0800

    color fixed

--- Visualize.py
+++ Visualize.py
@@ -11,11 +11,12 @@ SIZE_MAX = 50 ** 2
 NS = 50
 
 pos = np.random.uniform(0, 1, (NS, 2))
+color = np.ones((NS, 4)) * (0, 0, 0, 1)
 
 fig = plt.figure(num = "Bike Simulation", figsize=(6,6), facecolor='white')
 
 def init():
-    global color, size, scat
+    global size, scat
 
     # New axis over the whole figure, no frame and a 1:1 aspect ratio
     ax = fig.add_axes([0, 0, 1, 1], frameon = False, aspect = 1)
@@ -24,25 +25,25 @@ def init():
     #pos = np.random.uniform(0, 1, (NS, 2))
 
     # Ring colors
-    color = np.ones((NS, 4)) * (0, 0, 0, 1)
-    # Alpha color channel geos from 0(transparent) to 1(opaque)
-    color[:,3] = np.linspace(0, 1, NS)
+    # color = np.ones((NS, 4)) * (0, 0, 0, 1)
+    # Alpha color channel goes from 0(transparent) to 1(opaque)
+    # color[:,3] = np.linspace(0, 1, NS)
 
     # Ring sizes
     size = np.linspace(SIZE_MIN, SIZE_MAX, NS)
 
     # Scatter plot
-    scat = ax.scatter(pos[:,0], pos[:,1], s=size, lw=0.5, edgecolors=color, facecolors='None')
+    scat = ax.scatter(pos[:,0], pos[:,1], s=size, lw=0.5, edgecolors=color, facecolors=color)
 
     # Ensure limits are [0,1] and remove ticks
     ax.set_xlim(0, 1), ax.set_xticks([])
     ax.set_ylim(0, 1), ax.set_yticks([])
 
 def update(frame):
-    global color, size
+    global size
 
     # Every ring is made more transparnt
-    color[:, 3] = np.maximum(0, color[:,3]-1.0/NS)
+    # color[:, 3] = np.maximum(0, color[:,3]-1.0/NS)
 
     # Each ring is made larger
     size += (SIZE_MAX - SIZE_MIN) / NS
@@ -51,7 +52,7 @@ def update(frame):
     i = frame % 50
     # pos[i] = np.random.uniform(0, 1, 2)
     size[i] = SIZE_MIN
-    color[i, 3] = 1
+    # color[i, 3] = 1
 
     # Update scatter object
     scat.set_edgecolors(color)
