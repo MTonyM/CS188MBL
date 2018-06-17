7f6f5eee88d07bf6895e862d316b5e50e69ca0bd
Tujiao Dong <xiongzhq1997@gmail.com>
2018-06-17 20:58:31 +0800

    fix position

--- Visualize.py
+++ Visualize.py
@@ -10,16 +10,18 @@ SIZE_MIN = 50
 SIZE_MAX = 50 ** 2
 NS = 50
 
+pos = np.random.uniform(0, 1, (NS, 2))
+
 fig = plt.figure(num = "Bike Simulation", figsize=(6,6), facecolor='white')
 
 def init():
-    global pos, color, size, scat
+    global color, size, scat
 
     # New axis over the whole figure, no frame and a 1:1 aspect ratio
-    ax = fig.add_axes([0, 0, 1, 1], frameon=False, aspect=1)
+    ax = fig.add_axes([0, 0, 1, 1], frameon = False, aspect = 1)
 
     # Ring position
-    pos = np.random.uniform(0, 1, (NS, 2))
+    #pos = np.random.uniform(0, 1, (NS, 2))
 
     # Ring colors
     color = np.ones((NS, 4)) * (0, 0, 0, 1)
@@ -37,7 +39,7 @@ def init():
     ax.set_ylim(0, 1), ax.set_yticks([])
 
 def update(frame):
-    global pos, color, size
+    global color, size
 
     # Every ring is made more transparnt
     color[:, 3] = np.maximum(0, color[:,3]-1.0/NS)
@@ -47,7 +49,7 @@ def update(frame):
 
     # Reset specific ring
     i = frame % 50
-    pos[i] = np.random.uniform(0, 1, 2)
+    # pos[i] = np.random.uniform(0, 1, 2)
     size[i] = SIZE_MIN
     color[i, 3] = 1
 
